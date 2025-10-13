aedbc6dcb1bfa5b9e6a3cbf20779394a62c102b0
JP113z <josepablof113@gmail.com>
2025-10-13 14:44:15 -0600

    Guardar y cargar funcional

--- simplex.c
+++ simplex.c
@@ -12,36 +12,37 @@ GPtrArray *entry_var_names;
 GPtrArray *labels_obj_vars;
 GPtrArray *labels_con_vars;
 GtkWidget *label_nonneg;
+GtkWidget *combo_opt_global;
 
-GPtrArray *entry_obj_coefs; // coeficientes de Z
-GPtrArray *entry_con_coefs; // matriz de coeficientes
-GPtrArray *entry_rhs_list;  // lados derechos
-GPtrArray *combo_signs;     // signos
+GPtrArray *entry_obj_coefs;
+GPtrArray *entry_con_coefs;
+GPtrArray *entry_rhs_list;
+GPtrArray *combo_signs;
 
-// prototipos
+// --- Prototipos ---
 void on_generate_clicked(GtkButton *button, gpointer user_data);
 
-// Cerrar todo el programa cuando se cierra la ventana principal
+// --- Cerrar todo el programa ---
 void on_close_main_window(GtkWidget *widget, gpointer user_data)
 {
     gtk_main_quit();
 }
 
-// Cuando se cierra la segunda ventana directamente
+// --- Cuando se cierra la segunda ventana ---
 void on_second_window_destroy(GtkWidget *widget, gpointer user_data)
 {
-    gtk_widget_show_all(main_window);
     gtk_widget_hide(second_window);
+    gtk_widget_show_all(main_window);
 }
 
-// Botón "Cancelar"
+// --- Botón "Cancelar" ---
 void on_cancel_clicked(GtkButton *button, gpointer user_data)
 {
     gtk_widget_hide(second_window);
     gtk_widget_show_all(main_window);
 }
 
-// Restricciones triviales
+// --- Actualizar "Y con X1, X2 ≥ 0" ---
 void update_nonneg_label()
 {
     GString *vars_text = g_string_new("Y con ");
@@ -57,6 +58,7 @@ void update_nonneg_label()
     g_string_free(vars_text, TRUE);
 }
 
+// --- Actualizar nombres en la segunda ventana ---
 void on_var_name_changed(GtkEditable *editable, gpointer user_data)
 {
     int index = GPOINTER_TO_INT(user_data);
@@ -81,7 +83,7 @@ void on_var_name_changed(GtkEditable *editable, gpointer user_data)
     update_nonneg_label();
 }
 
-// Guardar problema a archivo
+// --- Guardar ---
 void on_save_clicked(GtkButton *button, gpointer user_data)
 {
     GtkWidget *dialog;
@@ -100,7 +102,14 @@ void on_save_clicked(GtkButton *button, gpointer user_data)
     gtk_file_filter_add_pattern(filter, "*.txt");
     gtk_file_filter_set_name(filter, "Archivos de texto (*.txt)");
     gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
-    gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), "modelo.txt");
+
+    const char *problem_name = g_object_get_data(G_OBJECT(button), "problem_name");
+    if (problem_name && strlen(problem_name) > 0)
+    {
+        char file_name[256];
+        snprintf(file_name, sizeof(file_name), "%s.txt", problem_name);
+        gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), file_name);
+    }
 
     if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT)
     {
@@ -126,7 +135,14 @@ void on_save_clicked(GtkButton *button, gpointer user_data)
         return;
     }
 
-    // Guardar cantidad de variables y restricciones
+    // Nombre del problema
+    fprintf(f, "%s\n", problem_name ? problem_name : "SinNombre");
+
+    // Guardar tipo de optimización (0 = Max, 1 = Min)
+    int opt_type = gtk_combo_box_get_active(GTK_COMBO_BOX(combo_opt_global));
+    fprintf(f, "%d\n", opt_type);
+
+    // Cantidad de variables y restricciones
     fprintf(f, "%u\n", entry_var_names->len);
     fprintf(f, "%u\n", labels_con_vars->len);
 
@@ -160,7 +176,7 @@ void on_save_clicked(GtkButton *button, gpointer user_data)
     g_free(filename);
 }
 
-// Cargar problema desde archivo
+// --- Cargar ---
 void on_load_clicked(GtkButton *button, gpointer user_data)
 {
     GtkWidget *dialog;
@@ -176,12 +192,10 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
 
     if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_OK)
         filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-    else
-    {
-        gtk_widget_destroy(dialog);
-        return;
-    }
+
     gtk_widget_destroy(dialog);
+    if (!filename)
+        return;
 
     FILE *f = fopen(filename, "r");
     if (!f)
@@ -190,17 +204,26 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
         return;
     }
 
+    char problem_name[128];
+    fgets(problem_name, sizeof(problem_name), f);
+    problem_name[strcspn(problem_name, "\r\n")] = 0;
+
+    int opt_type;
+    fscanf(f, "%d\n", &opt_type);
+
     int vars, cons;
     fscanf(f, "%d\n%d\n", &vars, &cons);
 
-    // Simular que el usuario los eligió
+    GtkWidget *entry_problem_name = GTK_WIDGET(gtk_builder_get_object(user_data, "entry_problem_name"));
+    gtk_entry_set_text(GTK_ENTRY(entry_problem_name), problem_name);
+
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(spin_vars), vars);
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(spin_constraints), cons);
 
-    // Llamar a generar la ventana
-    on_generate_clicked(NULL, NULL);
+    on_generate_clicked(NULL, user_data);
 
-    // Nombres
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_opt_global), opt_type);
+
     char buf[128];
     for (int i = 0; i < vars; i++)
     {
@@ -209,7 +232,6 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
         gtk_entry_set_text(GTK_ENTRY(g_ptr_array_index(entry_var_names, i)), buf);
     }
 
-    // Coeficientes de Z
     for (int i = 0; i < vars; i++)
     {
         fgets(buf, sizeof(buf), f);
@@ -217,7 +239,6 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
         gtk_entry_set_text(GTK_ENTRY(g_ptr_array_index(entry_obj_coefs, i)), buf);
     }
 
-    // Restricciones
     int idx = 0;
     for (int r = 0; r < cons; r++)
     {
@@ -227,7 +248,6 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
             buf[strcspn(buf, "\r\n")] = 0;
             gtk_entry_set_text(GTK_ENTRY(g_ptr_array_index(entry_con_coefs, idx++)), buf);
         }
-
         int sign;
         fscanf(f, "%d\n", &sign);
         GtkWidget *combo = g_ptr_array_index(combo_signs, r);
@@ -243,16 +263,31 @@ void on_load_clicked(GtkButton *button, gpointer user_data)
     g_free(filename);
 }
 
-// Generar modelo
+// --- Generar modelo ---
 void on_generate_clicked(GtkButton *button, gpointer user_data)
 {
+    GtkWidget *entry_problem_name_widget = GTK_WIDGET(gtk_builder_get_object(user_data, "entry_problem_name"));
+    const char *problem_name = gtk_entry_get_text(GTK_ENTRY(entry_problem_name_widget));
+
+    if (!problem_name || strlen(problem_name) == 0)
+    {
+        GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(main_window),
+                                                   GTK_DIALOG_MODAL,
+                                                   GTK_MESSAGE_WARNING,
+                                                   GTK_BUTTONS_OK,
+                                                   "Por favor, ingrese un nombre para el problema.");
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
+        return;
+    }
+
     int vars = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spin_vars));
     int cons = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spin_constraints));
 
     gtk_widget_hide(main_window);
 
     second_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(second_window), "Modelo generado");
+    gtk_window_set_title(GTK_WINDOW(second_window), problem_name);
     gtk_window_set_default_size(GTK_WINDOW(second_window), 600, 400);
     g_signal_connect(second_window, "destroy", G_CALLBACK(on_second_window_destroy), NULL);
 
@@ -260,14 +295,15 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
     gtk_container_add(GTK_CONTAINER(second_window), main_vbox);
     gtk_container_set_border_width(GTK_CONTAINER(main_vbox), 10);
 
-    entry_var_names = g_ptr_array_new_with_free_func(g_object_unref);
-    entry_obj_coefs = g_ptr_array_new_with_free_func(g_object_unref);
-    entry_con_coefs = g_ptr_array_new_with_free_func(g_object_unref);
-    entry_rhs_list = g_ptr_array_new_with_free_func(g_object_unref);
-    combo_signs = g_ptr_array_new_with_free_func(g_object_unref);
-    labels_obj_vars = g_ptr_array_new_with_free_func(g_object_unref);
+    entry_var_names = g_ptr_array_new();
+    entry_obj_coefs = g_ptr_array_new();
+    entry_con_coefs = g_ptr_array_new();
+    entry_rhs_list = g_ptr_array_new();
+    combo_signs = g_ptr_array_new();
+    labels_obj_vars = g_ptr_array_new();
     labels_con_vars = g_ptr_array_new();
 
+    // Nombres de variables
     GtkWidget *label_names = gtk_label_new("Nombres de las variables:");
     gtk_widget_set_halign(label_names, GTK_ALIGN_START);
     gtk_box_pack_start(GTK_BOX(main_vbox), label_names, FALSE, FALSE, 0);
@@ -279,22 +315,24 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
     {
         GtkWidget *entry = gtk_entry_new();
         char def_name[16];
-        snprintf(def_name, sizeof(def_name), "$X_%d$", i);
+        snprintf(def_name, sizeof(def_name), "X%d", i);
         gtk_entry_set_text(GTK_ENTRY(entry), def_name);
         gtk_entry_set_width_chars(GTK_ENTRY(entry), 5);
         gtk_box_pack_start(GTK_BOX(hbox_names), entry, FALSE, FALSE, 5);
         g_ptr_array_add(entry_var_names, entry);
     }
 
+    // Modelo
     GtkWidget *label_model = gtk_label_new("Modelo:");
+    gtk_widget_set_halign(label_model, GTK_ALIGN_START);
     gtk_box_pack_start(GTK_BOX(main_vbox), label_model, FALSE, FALSE, 0);
 
     GtkWidget *hbox_obj = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    GtkWidget *combo_opt = gtk_combo_box_text_new();
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_opt), "Maximizar");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_opt), "Minimizar");
-    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_opt), 0);
-    gtk_box_pack_start(GTK_BOX(hbox_obj), combo_opt, FALSE, FALSE, 5);
+    combo_opt_global = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_opt_global), "Maximizar");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo_opt_global), "Minimizar");
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_opt_global), 0);
+    gtk_box_pack_start(GTK_BOX(hbox_obj), combo_opt_global, FALSE, FALSE, 5);
     gtk_box_pack_start(GTK_BOX(hbox_obj), gtk_label_new("Z ="), FALSE, FALSE, 5);
 
     for (int i = 0; i < vars; i++)
@@ -308,8 +346,7 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
         gtk_box_pack_start(GTK_BOX(hbox_obj), entry_coef, FALSE, FALSE, 5);
         g_ptr_array_add(entry_obj_coefs, entry_coef);
 
-        const char *var_name = gtk_entry_get_text(GTK_ENTRY(g_ptr_array_index(entry_var_names, i)));
-        GtkWidget *label_var = gtk_label_new(var_name);
+        GtkWidget *label_var = gtk_label_new("");
         gtk_box_pack_start(GTK_BOX(hbox_obj), label_var, FALSE, FALSE, 5);
         g_ptr_array_add(labels_obj_vars, label_var);
     }
@@ -322,7 +359,7 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
     for (int r = 0; r < cons; r++)
     {
         GtkWidget *hbox_con = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-        GPtrArray *fila_labels = g_ptr_array_new_with_free_func(g_object_unref);
+        GPtrArray *fila_labels = g_ptr_array_new();
 
         for (int v = 0; v < vars; v++)
         {
@@ -335,8 +372,7 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
             gtk_box_pack_start(GTK_BOX(hbox_con), entry_coef, FALSE, FALSE, 5);
             g_ptr_array_add(entry_con_coefs, entry_coef);
 
-            const char *var_name = gtk_entry_get_text(GTK_ENTRY(g_ptr_array_index(entry_var_names, v)));
-            GtkWidget *label_var = gtk_label_new(var_name);
+            GtkWidget *label_var = gtk_label_new("");
             gtk_box_pack_start(GTK_BOX(hbox_con), label_var, FALSE, FALSE, 5);
             g_ptr_array_add(fila_labels, label_var);
         }
@@ -373,6 +409,8 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
     gtk_box_pack_end(GTK_BOX(hbox_buttons), btn_save, FALSE, FALSE, 5);
     gtk_box_pack_start(GTK_BOX(main_vbox), hbox_buttons, FALSE, FALSE, 10);
 
+    g_object_set_data_full(G_OBJECT(btn_save), "problem_name", g_strdup(problem_name), g_free);
+
     g_signal_connect(btn_cancel, "clicked", G_CALLBACK(on_cancel_clicked), NULL);
     g_signal_connect(btn_save, "clicked", G_CALLBACK(on_save_clicked), NULL);
 
@@ -385,6 +423,7 @@ void on_generate_clicked(GtkButton *button, gpointer user_data)
     gtk_widget_show_all(second_window);
 }
 
+// --- Main ---
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
@@ -394,11 +433,13 @@ int main(int argc, char *argv[])
     spin_vars = GTK_WIDGET(gtk_builder_get_object(builder, "spin_variables"));
     spin_constraints = GTK_WIDGET(gtk_builder_get_object(builder, "spin_constraints"));
     box_model = GTK_WIDGET(gtk_builder_get_object(builder, "box_model"));
+
     GtkWidget *btn_generate = GTK_WIDGET(gtk_builder_get_object(builder, "btn_generate"));
     GtkWidget *btn_load = GTK_WIDGET(gtk_builder_get_object(builder, "btn_load"));
-    g_signal_connect(btn_load, "clicked", G_CALLBACK(on_load_clicked), NULL);
+
+    g_signal_connect(btn_load, "clicked", G_CALLBACK(on_load_clicked), builder);
     g_signal_connect(main_window, "destroy", G_CALLBACK(on_close_main_window), NULL);
-    g_signal_connect(btn_generate, "clicked", G_CALLBACK(on_generate_clicked), NULL);
+    g_signal_connect(btn_generate, "clicked", G_CALLBACK(on_generate_clicked), builder);
 
     gtk_widget_show_all(main_window);
     gtk_main();
